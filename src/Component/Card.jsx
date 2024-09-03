60ab592a5c2c30b0ba6461e101e614fceb712389
Gaurav Divekar <101012709+Garry1106@users.noreply.github.com>
2024-09-03 19:56:25 +0530

    changes added

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({data}) => {
     return (
         <>
             <div className="card">
                 <div className="card-header">
-                    <img src="https://cdn11.bigcommerce.com/s-x49po/images/stencil/1280x1280/products/89260/155362/1668761597976_AAG_00077_A__09148.1669109977.jpg?c=2" />
+                    <img src={data.urlToImage} />
                 </div>
                 <div className="card-content">
-                    <h1 className="card-title">This is title</h1>
-                    <h3 className="card-source">End Gadget 11/06/2003</h3>
-                    <p className="card-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel perspiciatis excepturi obcaecati facere dolorum sed</p>
+                    <p>{data.id}</p>
+                    <h1 className="card-title">{data.title}</h1>
+                    <p className="card-desc">{data.description}</p>
                 </div>
             </div>
         </>
--- src/Component/Main.jsx
+++ src/Component/Main.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
-import Card from './Card' 
-import data from '../Data/response'
-const Main = () => {
+import React, { useEffect, useState } from "react";
+import Card from "../Component/Card";
 
-    const dataEle = data;
-    console.log(data)
-    return (
-        <div className='container'>
-            <Card />
-        </div>
-    )
-}
+const Main = () => {
+  const [data, setData] = useState([]);
+  const fetchData = async () => {
+    const response = await fetch(
+      `https://newsapi.org/v2/everything?q=India&apiKey=319f6625c4de4d569b0edc94d88f1592`
+    );
+    const variable = await response.json();
+    console.log(variable.articles);
+    setData(variable.articles);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <div className="container">
+      {data.map((item)=>{
+        return(
+          <Card key={item.id} data={item}/>
+        )
+      })}
+    </div>
+  );
+};
 export default Main;
